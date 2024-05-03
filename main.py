f639c07c7f290d55b6f7d6c124c0eed94ff8c1d1
JavidDitty <javidditty@gmail.com>
2024-05-02 23:42:53 -0400

    Add pytest tracing, replace templates with a yaml builder

--- main.py
+++ main.py
@@ -1,72 +1,6 @@
-import sys
-import site
-import platform
-import subprocess
-import importlib
-import importlib.metadata
 import argparse
-
-
-# Trace paths that appear in system calls during the target bash script to be traced's runtime
-def trace_paths(target):
-    command = f"strace -fy -ttt -qqq -s 4096 -e trace=%file bash {target} 2>&1" + "| awk -F'\"' '{print $2}' | grep -v '^\s*$'"
-    paths = str(subprocess.run(command, shell=True, capture_output=True).stdout, 'UTF-8').splitlines()
-    command = f"strace -fy -ttt -qqq -s 4096 -e trace=%file bash {target} 2>&1" + "| awk -F'[<>]' '{print $2}' | grep -v '^\s*$'"
-    paths += str(subprocess.run(command, shell=True, capture_output=True).stdout, 'UTF-8').strip().splitlines()
-    return list(set(paths))
-
-
-# Parse the module candidates found in the python package paths
-def parse_modules(paths):
-    module_paths = sys.path + [site.USER_BASE] + [site.USER_SITE]
-    modules = []
-    for path in paths:
-        for module_path in module_paths:
-            if module_path in path:
-                try:
-                    new_path = path.replace(module_path, '').split('/')[1].split('.')[0]
-                except:
-                    new_path = path.replace(module_path, '').split('/')[0].split('.')[0]
-                if new_path != '':
-                    modules.append(new_path)
-                break
-    modules = list(set(modules))
-    return modules
-
-
-# Select module candidates that are third-party python modules
-def parse_requirements(modules):
-    requirements = []
-    for module in modules:
-        try:
-            # Third-Party Python Modules
-            module_name = importlib.import_module(module).__name__
-            module_version = importlib.metadata.version(module_name)
-            requirements.append(f'{module_name}=={module_version}')
-        except importlib.metadata.PackageNotFoundError:
-            # Built-In Python Modules
-            pass
-        except ModuleNotFoundError:
-            # Files that are not Python Modules
-            pass
-    requirements.sort()
-    return requirements
-
-
- # Write requirements, or selected module candidates, to the requirements file
-def write_requirements(requirements, requirements_file="requirements.txt"):
-    with open(requirements_file, "w") as file:
-        file.writelines([f'{requirement}\n' for requirement in requirements])
-
-
-# Read a workflow configuration template, fill in its empty fields, and write it to workflow file
-def fill_template(template_file, workflow_file, workflow_name, python_version, requirements_file):
-    with open(template_file, "r") as file:
-        yaml = file.read()
-    yaml = yaml.format(workflow_name, python_version, requirements_file)
-
-    with open(workflow_file, "w") as file:
-        file.write(yaml)
+from tracing import Tracing
+from yamlci import YamlCI
 
 
 def parse_args():
@@ -76,22 +10,31 @@ def parse_args():
     parser.add_argument('--template', dest='template', type=str, help='path to a workflow configuration template', default='dependencies.yaml')
     parser.add_argument('--workflow', dest='workflow', type=str, help='path to, or for, a workflow configuration', default='workflow.yaml')
     parser.add_argument('--workflow_name', dest='workflow_name', type=str, help='name for a new workflow configuration', default='Workflow')
+    parser.add_argument('--new_trace', dest='new_trace', type=bool, help='whether the target should be traced again', default=False)
     return parser.parse_args()
 
 
 def main():
-    PYTHON_VERSION = platform.python_version()
+    # Parse runtime arguments
     args = parse_args()
-    paths = trace_paths(target=args.target)
-    modules = parse_modules(paths=paths)
-    requirements = parse_requirements(modules=modules)
-    write_requirements(requirements=requirements, 
-                       requirements_file=args.requirement)
-    fill_template(workflow_name=args.workflow_name, 
-                  python_version=PYTHON_VERSION, 
-                  template_file=args.template, 
-                  workflow_file=args.workflow,
-                  requirements_file=args.requirement)
+
+    # System trace a target program
+    tracing = Tracing(new_trace=args.new_trace,
+                      trace_log='trace.log',
+                      paths_log='paths.log',
+                      target=args.target)
+    
+    # Parse information from system trace
+    versions = tracing.parse_versions()
+    requirements = tracing.parse_requirements()
+    scripts = tracing.parse_script("pytest")
+
+    # Build CI/CD .yaml config from system tace information
+    ciyaml = YamlCI()
+    ciyaml.set_workflow_name(args.workflow_name)
+    ciyaml.set_workflow_trigger_branches(['main'])
+    ciyaml.set_jobs(versions, args.requirement, scripts)
+    ciyaml.dump(f'{args.workflow_name}.yaml')
 
 
 if __name__ == '__main__':
---
+++ tracing.py
@@ -0,0 +1,98 @@
+import sys
+import os
+import site
+import subprocess
+import importlib
+import importlib.metadata
+import re
+
+# Class that initiates and parses system traces of a program
+class Tracing:
+    def __init__(self,
+                 new_trace = False,
+                 trace_log = 'trace.log',
+                 paths_log = 'paths.log',
+                 target = 'target.sh'):
+        # Load the trace logs (or create it if it do not exist)
+        if new_trace or not os.path.exists(trace_log):
+            self.log_trace(target=target)
+        with open(trace_log, 'r') as log:
+            self.trace = list(set(log.read().splitlines()))
+
+        # Load the paths logs (or create it if it do not exist)
+        if new_trace or not os.path.exists(paths_log):
+            self.log_paths()
+        with open(paths_log, 'r') as log:
+            self.paths = log.read().splitlines()
+    
+    #========================================================================================================
+    #                                        GENERATE TRACE-RELATED LOGS
+    #========================================================================================================
+
+    # Trace the target and write the trace log to a file
+    def log_trace(self, target):
+        command = f'strace --follow-forks --decode-fds=path --trace=%file --output=trace.log bash {target} 2>&1'
+        subprocess.run(command, shell=True)
+
+    # Parse distinct paths from system trace and write them to a file
+    def log_paths(self):          
+        paths = []
+        for call in self.trace:
+            paths.extend(re.findall('\"(.+?)\"', call))
+            paths.extend(re.findall('<(.+?)>', call))
+        paths = list(set([path for path in paths if os.path.exists(path)]))
+        with open('paths.log', 'w') as log:
+            log.writelines("\n".join(paths))
+
+    #========================================================================================================
+    #                            PARSE CONFIGURATION INFORMATION FROM TRACE-RELATED LOGS
+    #========================================================================================================
+
+    # Parse language runtime versions
+    def parse_versions(self):
+        versions = []
+        path_versions = [re.findall('(?<=[\\/\\\\]python)(.+?)(?=[\\/\\\\])', path) for path in self.paths]
+        for path_version in path_versions:
+            versions.extend(path_version)
+        return list(set(versions))
+
+    # Parse requirements, or library/module dependencies
+    def parse_requirements(self):
+        # Parse module candidates found in the python package paths
+        module_paths = sys.path + [site.USER_BASE] + [site.USER_SITE]
+        modules = []
+        for path in self.paths:
+            for module_path in module_paths:
+                if module_path in path:
+                    try:
+                        new_path = path.replace(module_path, '').split('/')[1].split('.')[0]
+                    except:
+                        new_path = path.replace(module_path, '').split('/')[0].split('.')[0]
+                    if new_path != '':
+                        modules.append(new_path)
+                    break
+        modules = list(set(modules))
+        
+        # Select module candidates that are third-party python modules
+        requirements = []
+        for module in modules:
+            try:
+                # Third-Party Python Modules
+                module_name = importlib.import_module(module).__name__
+                module_version = importlib.metadata.version(module_name)
+                requirements.append(f'{module_name}=={module_version}')
+            except importlib.metadata.PackageNotFoundError:
+                # Built-In Python Modules
+                pass
+            except ModuleNotFoundError:
+                # Files that are not Python Modules
+                pass
+        requirements.sort()
+        return requirements
+
+    # Parse configuration of a script used in target
+    def parse_script(self, script):
+        execve = [list(eval(re.findall("\\[(.+?)\\]", trace)[0])) for trace in self.trace if "execve" in trace]
+        args = [arg for arg in execve if arg[0] == script]
+        args = [] if len(args) == 0 else args[0]
+        return args
