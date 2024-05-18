04c73a3a2b21255f877eca63dbd62b50eb419091
JavidDitty <javidditty@gmail.com>
2024-05-17 22:38:33 -0400

    Add docker tracing and yaml features

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def parse_args():
     parser.add_argument('--requirement', dest='requirement', type=str, help='path to, or for, a pip requirements file', default='requirement.txt')
     parser.add_argument('--template', dest='template', type=str, help='path to a workflow configuration template', default='dependencies.yaml')
     parser.add_argument('--workflow', dest='workflow', type=str, help='path to, or for, a workflow configuration', default='workflow.yaml')
-    parser.add_argument('--workflow_name', dest='workflow_name', type=str, help='name for a new workflow configuration', default='Workflow')
+    parser.add_argument('--workflow_name', dest='workflow_name', type=str, help='name for a new workflow configuration', default='workflow')
     parser.add_argument('--new_trace', dest='new_trace', type=bool, help='whether the target should be traced again', default=False)
+    parser.add_argument('--dockerfile', dest='dockerfile', type=str, help='path to a dockerfile', default='Dockerfile')
     return parser.parse_args()
 
 
@@ -27,7 +28,24 @@ def main():
     # Parse information from system trace
     versions = tracing.parse_versions()
     requirements = tracing.parse_requirements()
-    scripts = tracing.parse_script("pytest")
+    with open('requirements.txt', 'w') as file:
+        file.write('\n'.join(requirements))
+    scripts = dict()
+
+    # Parse scripts from system trace
+    scripts['pytest'] = tracing.parse_script('pytest')
+    scripts['docker'] = {'exec': []}
+    for parse in tracing.parse_script("docker"):
+        if 'exec' in parse:
+            scripts['docker']['exec'].append(parse)
+
+    scripts['docker']['img'] = ''
+    if len(scripts['docker']['exec']) != 0:
+        with open(args.dockerfile, 'r') as file:
+            dockerfile = file.readlines()
+            for line in dockerfile:
+                if line.startswith('FROM'):
+                    scripts['docker']['img'] = line[5:]
 
     # Build CI/CD .yaml config from system tace information
     ciyaml = YamlCI()
--- yamlci.py
+++ yamlci.py
@@ -14,17 +14,47 @@ class YamlCI:
         self.yaml.update({'on': {'push': {'branches': branches}}})
 
     # Set the jobs in the yaml file, including runtime versions, requirements, and a pytest invocation
-    def set_jobs(self, versions: list, requirements: str, pytest: list[str]):
-        self.yaml.update({'jobs': {'build': {'runs-on': 'ubuntu-latest', 'strategy': {'matrix': {'python-version': versions}}, 'steps': [ 
-            {'uses': 'actions/checkout@v4'}, 
-            {'name': 'Setup Python ${{ matrix.python-version }}',
-             'uses': 'actions/setup-python@v4',
-             'with': {'python-version': '${{ matrix.python-version }}'}},
-            {'name': 'Install Dependencies',
-             'run': f'python -m pip install --upgrade pip; pip install -r {requirements}'},
-            {'name': 'Run Pytest',
-             'run': f'pip install pytest; %s' % ' '.join(pytest)}
-        ]}}})
+    def set_jobs(self, versions: list, requirements: str, scripts: dict):
+        # TODO: Decompose this function into smaller, specialized functions
+
+        # Add jobs to configuration
+        self.yaml.update({'jobs': {}})
+
+        # Add container job
+        if scripts['docker']['img'] != '':
+            self.yaml['jobs'].update({'container': {'runs-on': 'ubuntu-latest'}})
+
+            # Add container to container job
+            if len(scripts['docker']['img']) != 0: # Add container img
+                self.yaml['jobs']['container'].update({'container': {'image': scripts['docker']['img']}})
+
+            # Add steps to container job
+            self.yaml['jobs']['container'].update({'steps': [{'uses': 'actions/checkout@v4'}]})
+
+            # Add execs to container job
+            if len(scripts['docker']['img']) != 0 and len(scripts['docker']['exec']) != 0: # Add container execs
+                docker = '; '.join([' '.join(script[6:]) for script in scripts['docker']['exec']])
+                self.yaml['jobs']['container']['steps'].append({
+                    'name': 'Execute docker jobs',
+                    'run': docker})
+        
+        # Add basic job
+        if len(versions) != 0: 
+            self.yaml['jobs'].update({'basic': {'runs-on': 'ubuntu-latest'}})
+            self.yaml['jobs']['basic'].update({'strategy': {'matrix': {'python-version': versions}}}) # Add python-versions
+            self.yaml['jobs']['basic'].update({'steps': [{'uses': 'actions/checkout@v4'}]}) # Add steps to basic job
+
+            # Add pytest to job
+            if len(scripts['pytest'][0]) != 0:
+                pytest = '; '.join([' '.join(script) for script in scripts['pytest']])
+                self.yaml['jobs']['basic']['steps'].extend( # Add setup-python and install dependencies
+                    [{'name': 'Setup Python ${{ matrix.python-version }}',
+                    'uses': 'actions/setup-python@v4',
+                    'with': {'python-version': '${{ matrix.python-version }}'}},
+                    {'name': 'Install Dependencies',
+                    'run': f'python -m pip install --upgrade pip; pip install -r {requirements}'}])
+                self.yaml['jobs']['basic']['steps'].append({'name': 'Run Pytest', 'run': f'pip install pytest; {pytest}'}) # Add pytest
+
 
     # Dump the yaml file, as it has been built, to a file
     def dump(self, path: str):
