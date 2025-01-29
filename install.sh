0a7709524517c7fc010971cd7caa54a690ec148c
JavidDitty <javidditty@gmail.com>
2025-01-28 22:41:19 -0500

    Fix requirements.txt selection in install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ MODULE_DIR=$(realpath "$TOOL_DIR/.modules")
 if [ ! -d "$MODULE_DIR" ]; then
     python3 -m venv $MODULE_DIR
     source $(realpath "$MODULE_DIR/bin/activate")
-    pip install --no-input -r requirements.txt
+    pip install --no-input -r $(realpath "$TOOL_DIR/requirements.txt")
     deactivate
 fi
